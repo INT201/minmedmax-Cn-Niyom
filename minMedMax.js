bbba6448f3813c76b3666e4a1f028301d50b0904
Chalunthotn Niyom <Chalunthorn.niyom@gmail.com>
2022-09-04 23:08:17 +0700

    ส่งครั้งที่1

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,27 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+    let minMedMaxResult = { min: n1, mid: n2, max: n3 };
+    let tem;
+    if(minMedMaxResult.min > minMedMaxResult.mid)
+    {
+        tem = minMedMaxResult.min;
+        minMedMaxResult.min = minMedMaxResult.mid;
+        minMedMaxResult.mid = tem;
+    }
+    if(minMedMaxResult.min > minMedMaxResult.max)
+    {
+        tem = minMedMaxResult.min;
+        minMedMaxResult.min = minMedMaxResult.max;
+        minMedMaxResult.max = tem;
+    }
+    if(minMedMaxResult.mid > minMedMaxResult.max)
+    {
+        tem = minMedMaxResult.max;
+        minMedMaxResult.max = minMedMaxResult.mid;
+        minMedMaxResult.mid = tem;
+    }
+    return minMedMaxResult;
 }
+console.log(minMedMax(4,3,2));
 module.exports = minMedMax
